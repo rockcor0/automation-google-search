2a220b69f6b7aa713bac8a4d4b1857e670446b72
Ricardo Delgado <ridel007@gmail.com>
2020-12-19 19:22:39 -0500

    Config the ChromeDriver

--- src/test/java/com/rockcor/GoogleSearchTest.java
+++ src/test/java/com/rockcor/GoogleSearchTest.java
@@ -3,10 +3,26 @@
  */
 package com.rockcor;
 
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 /**
  * @author ricardodelgadocarreno
  *
  */
 public class GoogleSearchTest {
+	
+	//WebDriver from Selenium
+	private WebDriver driver;
+	
+	@Before
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver", "./src/test/resources/cromedriver/chromedriver");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+	}
+	
 
 }
