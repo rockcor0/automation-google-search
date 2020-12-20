1e01ee14a98faa5cb588c365f78da3e7961ee9e7
Ricardo Delgado <ridel007@gmail.com>
2020-12-19 19:34:09 -0500

    Creating the test case

--- src/test/java/com/rockcor/GoogleSearchTest.java
+++ src/test/java/com/rockcor/GoogleSearchTest.java
@@ -3,8 +3,16 @@
  */
 package com.rockcor;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
@@ -12,6 +20,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
  *
  */
 public class GoogleSearchTest {
+	
+	public final String SEARCH = "I think we are alone now";
 	
 	//WebDriver from Selenium
 	private WebDriver driver;
@@ -22,6 +32,22 @@ public class GoogleSearchTest {
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com/");
+	}
+	
+	@Test
+	public void testGooglePage() {
+		WebElement sBox = driver.findElement(By.name("q"));
+		sBox.clear();
+		sBox.sendKeys(SEARCH);
+		sBox.submit();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		assertEquals(SEARCH, driver.getTitle());
+	}
+	
+	@After
+	public void tearDown() {
+		driver.quit();
 	}
 	
 
